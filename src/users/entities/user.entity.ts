e31a3bb73edd31faeb4b5017034f09537ac03d30
johnjoker13 <joaoavelino2008@hotmail.com>
2022-11-07 12:55:00 -0300

    feat(src/users/entities): creates an entity for user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn('increment')
   id: number;
+
+  @Column()
   nome: string;
+
+  @Column()
   idade: number;
 }
