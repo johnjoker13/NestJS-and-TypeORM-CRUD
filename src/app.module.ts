0e90ca56ef4e4562f6743eaf016f0e2113c50611
johnjoker13 <joaoavelino2008@hotmail.com>
2022-11-07 17:12:39 -0300

    refactor(src/app.modules && src/users/modules/users.module): moves users.module file to modules dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { config } from './config/ormconfig';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/modules/users.module';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forRoot(config)],
