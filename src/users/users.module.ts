324bf3914a49640da41287eb64da06328491afb0
johnjoker13 <joaoavelino2008@hotmail.com>
2022-11-07 16:55:43 -0300

    refactor(user.controller/module): moves files to moves file to controller dir

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './services/users.service';
-import { UsersController } from './users.controller';
+import { UsersController } from './controllers/users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
