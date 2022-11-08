4bf9a8873a33042c773c042f4234358756cb3975
johnjoker13 <joaoavelino2008@hotmail.com>
2022-11-08 15:12:52 -0300

    feat(src/utils/mocks/index): creates index for mocks exportations

---
+++ src/utils/mocks/index.ts
@@ -0,0 +1,6 @@
+import USERS from './allUsersMock';
+import USER from './userByIdMock';
+import USERUPDATED from './userUpdatedMock';
+import USERDELETED from './userDeletedMock';
+
+export { USERS, USER, USERUPDATED, USERDELETED };
