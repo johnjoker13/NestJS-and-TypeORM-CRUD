41cbf403e5c9449c18133d7624b9dab17a1900c1
johnjoker13 <joaoavelino2008@hotmail.com>
2022-11-21 16:33:36 -0300

    feat(src/common/exceptions/http-exception-filters.ts): creates a global exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AllExceptionsFilter } from './common/exceptions/http-exception.filters';
 import { config } from './config/ormconfig';
 import { UsersModule } from './users/modules/users.module';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/exceptions/http-exception.filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   await app.listen(3000);
 }
+
 bootstrap();
