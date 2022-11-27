45ec97a3b7ce16fa4a6c359a8867045c6d1b0cd3
johnjoker13 <joaoavelino2008@hotmail.com>
2022-11-26 21:59:40 -0300

    refactor(src/main.ts): changes api listening door

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-  await app.listen(3000);
+  await app.listen(3001);
 }
 
 bootstrap();
